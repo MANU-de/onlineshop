07fcc536bfc6d908cf474318a5fc7e6bc775f781
Manuela Schrittwieser <manuelaschrittwieser@yahoo.com>
2022-05-16 09:13:22 +0200

    Erstes Layout

---
+++ card.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <img src="camera-g25fb7966d_1920.jpg" class="card-img-top" alt="" height="200"
+     width="300">
+     <div class="card-body">
+        Testprodukt, hier eine Beschreibung!
+        </div>
+        <div class="card_footer">
+         <a href="" class="btn btn-primary btn-sm">details</a>
+         <a href="" class="btn btn-success btn-sm">In den Warenkorp</a>
+        </div>
+        </div>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'on');
+?>
+<!Doctype html>
+<html lang="de">
+    <head>
+        <title>
+            Pictures Shop
+        </title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/styles.css">
+    </head>
+    <body>
+        <header class="jumpotron">
+            <div class="container">
+            <h1>Welcome to the Pictures Shop</h1>
+            </div>
+        </header>
+        <section class="container" id="products">
+            <div class="row">
+                <div class="col">
+                <?php include 'card.php'?>
+                </div>
+                <div class="col">
+                <?php include 'card.php'?>
+                </div>
+                <div class="col">
+                <?php include 'card.php'?>
+                </div>
+                <div class="col">
+                <?php include 'card.php'?>
+            </div>
+            </section>
+
+
+
+      <script src="assets/js/bootstrap.bundle.js"></script>
+    </body>
+</html>
